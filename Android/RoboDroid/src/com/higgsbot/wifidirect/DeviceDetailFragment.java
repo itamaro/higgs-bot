cfeb1c51da1ea5f72d23e5bd0e4f515208be50cc
Eliel Haouzi <eliel.haouzi@gmail.com>
2013-12-23 21:49:20 +0200

    update the manifest file (add permission and activity)

---
+++ Android/RoboDroid/src/com/higgsbot/wifidirect/DeviceDetailFragment.java
@@ -0,0 +1,214 @@
+package com.higgsbot.wifidirect;
+
+import android.app.Fragment;
+import android.app.ProgressDialog;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pInfo;
+import android.net.wifi.p2p.WifiP2pManager.ConnectionInfoListener;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.higgsbot.wifidirect.DeviceListFragment.DeviceActionListener;
+import com.higgsbot.robodroid.R;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * A fragment that manages a particular peer and allows interaction with device
+ * i.e. setting up network connection and transferring data.
+ */
+public class DeviceDetailFragment extends Fragment implements ConnectionInfoListener {
+
+	private View mContentView = null;
+    private WifiP2pDevice device;
+    private WifiP2pInfo info;
+    ProgressDialog progressDialog = null;
+    private Boolean mMustStop = false;
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        mContentView = inflater.inflate(R.layout.device_detail, null);
+
+        mContentView.findViewById(R.id.btn_disconnect).setOnClickListener(
+                new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        ((DeviceActionListener) getActivity()).disconnect();
+                    }
+                });
+
+        return mContentView;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        this.mMustStop = true; // Stop the infinite loop
+    }
+    
+    @Override
+    public void onConnectionInfoAvailable(final WifiP2pInfo info) {
+        if (progressDialog != null && progressDialog.isShowing()) {
+            progressDialog.dismiss();
+        }
+        
+        this.info = info;
+        this.getView().setVisibility(View.VISIBLE);
+        
+        // The owner IP is now known.
+        TextView view = (TextView) mContentView.findViewById(R.id.group_owner);
+        view.setText(getResources().getString(R.string.group_owner_text)
+                + ((info.isGroupOwner == true) ? getResources().getString(R.string.yes)
+                        : getResources().getString(R.string.no)));
+
+        // InetAddress from WifiP2pInfo structure.
+        view = (TextView) mContentView.findViewById(R.id.device_info);
+        view.setText("Group Owner IP - " + info.groupOwnerAddress.getHostAddress());
+        
+        // hide the connect button
+        mContentView.findViewById(R.id.btn_connect).setVisibility(View.GONE);
+        
+        if (info.groupFormed && info.isGroupOwner) {
+        	
+        	new DriverAsyncTask(this).execute();
+            new ArmControlAsyncTask(this).execute();
+
+        } 
+    }
+
+    /**
+     * Updates the UI with device data
+     * 
+     * @param device the device to be displayed
+     */
+    public void showDetails(WifiP2pDevice device) {
+        this.device = device;
+        this.getView().setVisibility(View.VISIBLE);
+        TextView view = (TextView) mContentView.findViewById(R.id.device_address);
+        view.setText(device.deviceAddress);
+        view = (TextView) mContentView.findViewById(R.id.device_info);
+        view.setText(device.toString());
+    }
+
+    /**
+     * Clears the UI fields after a disconnect or direct mode disable operation.
+     */
+    public void resetViews() {
+        mContentView.findViewById(R.id.btn_connect).setVisibility(View.VISIBLE);
+        TextView view = (TextView) mContentView.findViewById(R.id.device_address);
+        view.setText(R.string.empty);
+        view = (TextView) mContentView.findViewById(R.id.device_info);
+        view.setText(R.string.empty);
+        view = (TextView) mContentView.findViewById(R.id.group_owner);
+        view.setText(R.string.empty);
+        view = (TextView) mContentView.findViewById(R.id.status_text);
+        view.setText(R.string.empty);
+        this.getView().setVisibility(View.GONE);
+    }
+
+    /**
+     * A simple server socket that accepts connection and writes some data on
+     * the stream.
+     */
+    
+    public static class DriverAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        private DeviceDetailFragment activity;
+
+        
+        public DriverAsyncTask(DeviceDetailFragment activity) {
+            this.activity = activity;
+        }
+
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            String msg = null;
+            ServerSocket serverSocket = null;
+            
+        	try {
+        		serverSocket = new ServerSocket(8988);
+                Log.d(WiFiDirectActivity.TAG, "Server: Socket opened");
+        	 } catch (IOException e) {
+                 Log.e(WiFiDirectActivity.TAG, e.getMessage());
+             }
+            
+            while (!this.activity.mMustStop) {
+            	try {
+            		Socket client = serverSocket.accept();
+	                Log.d(WiFiDirectActivity.TAG, "Server: connection done");
+	                
+	            	InputStream inputstream = client.getInputStream();
+	            	byte[] buffer = new byte[1024];
+	            	inputstream.read(buffer);
+	            	//inputstream.close();
+	            	msg = new String(buffer);
+	            	
+	            	Log.d("DeviceDetailFragment", "Driver Socket message: " + msg);
+	            	/* Call here to the class audio */
+	            	
+	            } catch (IOException e) {
+	                Log.e(WiFiDirectActivity.TAG, e.getMessage());
+	            }
+            }
+            return null;
+        }
+    }
+    
+    public static class ArmControlAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        private DeviceDetailFragment activity;
+
+        
+        public ArmControlAsyncTask(DeviceDetailFragment activity) {
+            this.activity = activity;
+        }        
+        @Override
+        protected Void doInBackground(Void... params) {
+            String msg = null;
+            ServerSocket serverSocket = null;
+            
+        	try {
+        		serverSocket = new ServerSocket(8989);
+                Log.d(WiFiDirectActivity.TAG, "Server: Socket opened");
+        	 } catch (IOException e) {
+                 Log.e(WiFiDirectActivity.TAG, e.getMessage());
+             }
+            
+            while (!this.activity.mMustStop) {
+            	try {
+            		Socket client = serverSocket.accept();
+	                Log.d(WiFiDirectActivity.TAG, "Server: connection done");
+	                
+	            	InputStream inputstream = client.getInputStream();
+	            	byte[] buffer = new byte[1024];
+	            	inputstream.read(buffer);
+	            	//inputstream.close();
+	            	msg = new String(buffer);
+	            	
+	            	Log.d("DeviceDetailFragment", "Arm Control Socket message: " + msg);
+	            	/* Call here to the class audio */
+	            	
+	            } catch (IOException e) {
+	                Log.e(WiFiDirectActivity.TAG, e.getMessage());
+	            }
+            }
+            return null;
+        }
+    }
+}
