b8f96697601a4207d53214a78b65b2b4b2c92b82
Gogofo <gogofo@gmail.com>
2013-12-24 22:50:30 +0200

    adding protocol support

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -4,38 +4,75 @@
 #include "SimpleControl.h"
 #include "higgsmodem.h"
 
-RobotArm arm(6);
+RobotArm Arm(6);
 MotorController MtrCtrl(10, 3, 9, 8, 5, 11, 12);
 RobotMovement Mvmnt(&MtrCtrl);
 
 SimpleControl Knife(7);
 SimpleControl Nitro(4);
 
+// Speed levels
+int WheelSpeeds[] = {0, 36, 72, 108, 144, 180, 216, 255};
+int ArmSpeeds[] = {0, 12, 24, 36, 48, 60, 72, 90};
+
 void setup()
 {
     Mvmnt.Setup();
-    arm.Setup();
+    Arm.Setup();
     Knife.Setup();
     Nitro.Setup();
     
     Knife.Off();
     Nitro.Off();
-    
-    Serial.begin(9600);
-    Serial.println("I'm listening");
 }
 
 void loop()
 {
-      int msg_len = getAndroidMessage();
-      Serial.print("Got message of length ");
-      Serial.println(msg_len);
-      for (int i=0; i < msg_len; ++i) 
-      {
-    Serial.println((unsigned char)AndroidMessage[i], HEX);
-    delay(1);
-      }
-      Serial.println(AndroidMessage);
+    int msg_len = getAndroidMessage();
+    
+    if (msg_len != 2)
+    {
+        // TODO: ERROR? HOW?
+    }
+    else
+    {
+        /****************** Parse ******************/
+        // LeftDir(1) + LeftSpeed(3) + RightDir(1) + RightSpeed(3) + ArmDir(1) + ArmSpeed(3) + Knife(1) + Nitro(1) + Padding(2)
+        int LDir = (AndroidMessage[0] & 0x80) >> 7;
+        int LSpeed = (AndroidMessage[0] & 0x70) >> 4;
+        int RDir = (AndroidMessage[0] & 0x08) >> 3;
+        int RSpeed = (AndroidMessage[0] & 0x07) >> 0;
+        
+        int ADir = (AndroidMessage[1] & 0x80) >> 7;
+        int ASpeed = (AndroidMessage[1] & 0x70) >> 4;
+        int K = (AndroidMessage[1] & 0x08) >> 3;
+        int N = (AndroidMessage[1] & 0x04) >> 2;
+        int Padding = (AndroidMessage[1] & 0x03) >> 0;
+        
+        /****************** Handle ******************/
+        Mvmnt.MoveWheel(LeftWheel, WheelSpeeds[LSpeed], LDir);
+        Mvmnt.MoveWheel(RightWheel, WheelSpeeds[RSpeed], RDir);
+        
+        Arm.SetSpeed(ArmSpeeds[ASpeed], ADir);
+        
+        if (K == 0)
+        {
+            Knife.Off();
+        }
+        else
+        {
+            Knife.On();
+        }
+        
+        if (N == 0)
+        {
+            Nitro.Off();
+        }
+        else
+        {
+            Nitro.On();
+        }
+    }
 }
 
 
