05b08c6311e46295cdbb3f137bdb77c4a2aab63f
Gogofo <gogofo@gmail.com>
2013-12-24 21:35:27 +0200

    higgs modem is now added to the sketch. It is now possible (and should be easy) to write a matching protocol to the android app

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -2,6 +2,7 @@
 #include "ArmLogic.h"
 #include "MovementLogic.h"
 #include "SimpleControl.h"
+#include "higgsmodem.h"
 
 RobotArm arm(6);
 MotorController MtrCtrl(10, 3, 9, 8, 5, 11, 12);
@@ -19,11 +20,27 @@ void setup()
     
     Knife.Off();
     Nitro.Off();
+    
+    Serial.begin(9600);
+    Serial.println("I'm listening");
 }
 
 void loop()
 {
- /*   
+      int msg_len = getAndroidMessage();
+      Serial.print("Got message of length ");
+      Serial.println(msg_len);
+      for (int i=0; i < msg_len; ++i) 
+      {
+    Serial.println((unsigned char)AndroidMessage[i], HEX);
+    delay(1);
+      }
+      Serial.println(AndroidMessage);
+}
+
+
+
+/****************** SOME USAGE EXAMPLES ****************************
     Mvmnt.MoveForward(255);
     delay(1000);
     Mvmnt.MoveBackward(255);
@@ -32,8 +49,7 @@ void loop()
     delay(1000);
     Mvmnt.SpinRight(255);
     delay(1000);
-    */
- 
+
  #if 0
  arm.RawWrite(90);
  #else
@@ -51,12 +67,10 @@ void loop()
     arm.RawWrite(90);
     delay(1000);
  #endif
-    /*
     arm.RawWrite(800);
     delay(1000);
     arm.RawWrite(1500);
     delay(1000);
     arm.RawWrite(2200);
     delay(1000);
-    */
-}
+*******************************************************************/
