073613f9bd89229d26c97119433bc13de1c89139
Itamar O <itamarost@gmail.com>
2014-01-01 21:58:21 +0200

    debugging code

--- Android/RoboDroid/src/com/higgsbot/robodroid/MainActivity.java
+++ Android/RoboDroid/src/com/higgsbot/robodroid/MainActivity.java
@@ -1,8 +1,11 @@
 package com.higgsbot.robodroid;
 
+import com.higgsbot.wifidirect.AudioModem;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -18,6 +21,11 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public void playTestSound(View view) {
+    	AudioModem modem = new AudioModem();
+    	modem.sendData("Go Higgs abcdefg0123456789 yo yo yo yo".toCharArray());
     }
     
 }
